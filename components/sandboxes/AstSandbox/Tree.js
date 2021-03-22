e1b6b2c645bbef85ca06565b1e376fbf2ff20737
Narendra Syahrasyad <nanda.s@hey.com>
2021-03-22 11:48:35 -0700

    refactor: add comments to Tree component

--- components/sandboxes/AstSandbox/Tree.js
+++ components/sandboxes/AstSandbox/Tree.js
@@ -6,6 +6,13 @@ const TreeContext = React.createContext()
 
 const useTreeContext = () => React.useContext(TreeContext)
 
+/**
+ * The Tree component is an interactive AST. Given a `tree` and a `depth`, this
+ * component renders all tree nodes up to the given depth. Deeper nodes can be
+ * toggled by clicking on the node labels.
+ *
+ * The given tree is assumed to be an AST produced by @babel/parse.
+ */
 export default function Tree({ className, tree, depth = 2 }) {
   const visibleKeys = Object.entries(tree).filter(isVisible)
   return (
@@ -21,11 +28,22 @@ export default function Tree({ className, tree, depth = 2 }) {
   )
 }
 
+/**
+ * An Entry represents a single key-value pair in an AST node, uniquely identified
+ * by its path from the root node.
+ *
+ * If the given value is an object or array, this component renders recursively.
+ * Otherwise, the component renders a simple key -> value pair.
+ */
 function Entry({ item: [key, value], path = [], depth }) {
   const { depth: initialDepth } = useTreeContext()
   const [isOpen, setIsOpen] = React.useState(depth <= initialDepth)
   const newPath = [...path, key]
 
+  /**
+   * For arrays, the key of each item is its index. This doesn't look quite nice, so
+   * we change the key to its AST node type.
+   */
   if (Array.isArray(value)) {
     return (
       <ListItem>
@@ -87,6 +105,8 @@ function Entry({ item: [key, value], path = [], depth }) {
   )
 }
 
+// -- Helper components --
+
 function ListItem(props) {
   return (
     <motion.li
@@ -127,12 +147,32 @@ function ExpandIcon(props) {
   )
 }
 
+// -- Helper functions --
+
+/**
+ * Properties in the whitelist provide context — e.g. for an Identifier node, we also
+ * want to know what the name of the Identifier is.
+ */
+const Whitelist = new Set(['type', 'kind', 'value', 'name', 'operator'])
+
+/**
+ * Given a property of an AST node, check if that property should be rendered or not.
+ * Generally speaking, we only want to render if the property is in the whitelist or
+ * if the property is itself an AST node.
+ */
 function isVisible([key, value]) {
-  const whitelist = new Set(['type', 'kind', 'value', 'name', 'operator'])
-  if (whitelist.has(key)) {
+  if (Whitelist.has(key)) {
     return true
   }
+
+  /**
+   * typeof null === 'object', so we have to check if the value is truthy
+   * before checking if it's an object or not.
+   */
   if (value && typeof value === 'object') {
+    /**
+     * Only render the array if it's an array of AST nodes.
+     */
     if (Array.isArray(value)) {
       const [first] = value
       if (first && typeof first === 'object' && !Array.isArray(first)) {
@@ -143,6 +183,7 @@ function isVisible([key, value]) {
       return value.hasOwnProperty('type')
     }
   }
+
   return false
 }
 
@@ -153,6 +194,8 @@ function toText(item) {
   }
   return lookup[typeof item]
 }
+
+// -- Styled components --
 
 const List = styled(motion.ul).attrs({ layout: 'position' })`
   list-style: none !important;
