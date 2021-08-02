d7361e53eb40b03493b2cb0c48b522a744067dbc
Nanda Syahrasyad <nanda.s@hey.com>
2021-08-02 16:34:36 -0700

    feat(07): update h3 and ol styles (#27)

--- elements/OrderedList.js
+++ elements/OrderedList.js
@@ -1,12 +1,36 @@
+import React from 'react'
 import { styled } from 'twin.macro'
 
-const OrderedList = styled.ol`
-  list-style-type: decimal;
-  padding-left: 16px;
+export default function OrderedList({ children }) {
+  return (
+    <ListWrapper>
+      {React.Children.map(children, (child, index) => (
+        <ListItem key={index}>
+          <div>{child.props.children}</div>
+        </ListItem>
+      ))}
+    </ListWrapper>
+  )
+}
+
+const ListWrapper = styled.ol`
+  list-style: none;
+  counter-reset: counts 0;
 
   > :not([hidden]) ~ :not([hidden]) {
     margin-top: 8px;
   }
 `
 
-export default OrderedList
+const ListItem = styled.li`
+  counter-increment: counts 1;
+  display: flex;
+
+  &:before {
+    content: counters(counts, '.') '. '; // {count}.
+    padding-right: 16px;
+    font-family: var(--text-mono);
+    font-weight: 500;
+    color: var(--gray600);
+  }
+`
