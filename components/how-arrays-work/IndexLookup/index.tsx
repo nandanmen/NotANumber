dbab8b380e2335644c2ad84da6744c148d8287c2
Nanda Syahrasyad <nanda.s@hey.com>
2021-09-25 12:22:25 -0700

    feat: update index visualizer with index 500

--- components/how-arrays-work/IndexLookup/index.tsx
+++ components/how-arrays-work/IndexLookup/index.tsx
@@ -1,22 +1,40 @@
 import React from 'react'
 import { styled } from '@stitches/react'
 
-import ActiveIndexList from './ActiveIndexList'
+import { ArrayList, ArrayListItem } from '../ArrayList'
 
-type IndexLookupProps = {
-  items: unknown[]
-}
+const items = [1, 2, 3]
+const indexValues = [0, 1, 500]
 
-export default function IndexLookup({ items }: IndexLookupProps) {
-  const [activeIndex, setActiveIndex] = React.useState(0)
+export default function IndexLookup() {
+  const [currentIndex, setCurrentIndex] = React.useState(0)
+  const activeIndex = indexValues[currentIndex]
   return (
     <Wrapper>
       <IndexButton
-        onClick={() => setActiveIndex((index) => (index + 1) % items.length)}
+        onClick={() =>
+          setCurrentIndex((index) => (index + 1) % indexValues.length)
+        }
       >
         Index: {activeIndex}
       </IndexButton>
-      <ActiveIndexList items={items} activeIndex={activeIndex} />
+      <ArrayList>
+        {items.map((item, index) => {
+          const isActive = activeIndex === index
+          return (
+            <ItemWithIndex isActive={isActive} index={index}>
+              {item}
+            </ItemWithIndex>
+          )
+        })}
+        <Spacer />
+        <ItemWithIndex isActive={activeIndex === 499} index={499}>
+          498
+        </ItemWithIndex>
+        <ItemWithIndex isActive={activeIndex === 500} index={500}>
+          499
+        </ItemWithIndex>
+      </ArrayList>
     </Wrapper>
   )
 }
@@ -34,4 +52,61 @@ const IndexButton = styled('button', {
   border: '2px solid var(--border-color)',
   background: 'var(--teal)',
   borderRadius: '6px',
+})
+
+// --
+
+function ItemWithIndex({ isActive, index, children }) {
+  return (
+    <div>
+      <ArrayListItem
+        pressed={isActive}
+        variant={isActive ? 'highlight' : undefined}
+        shadow
+      >
+        {children}
+      </ArrayListItem>
+      <Index active={isActive}>{index}</Index>
+    </div>
+  )
+}
+
+const Index = styled('div', {
+  textAlign: 'center',
+  marginTop: '12px',
+  variants: {
+    active: {
+      true: {
+        color: 'var(--color-highlight-secondary)',
+        fontWeight: 600,
+      },
+    },
+  },
+})
+
+// --
+
+function Spacer() {
+  return (
+    <SpacerWrapper>
+      <Dot />
+      <Dot />
+      <Dot />
+    </SpacerWrapper>
+  )
+}
+
+const SpacerWrapper = styled('div', {
+  paddingTop: 'calc(2rem - 8px)',
+  margin: '0 20px',
+  display: 'flex',
+  gap: '8px',
+})
+
+const Dot = styled('div', {
+  $$size: '16px',
+  width: '$$size',
+  height: '$$size',
+  background: 'var(--gray400)',
+  borderRadius: '50%',
 })
