62a41e8239d60a43dd31458b89f7ac47c83db151
Nanda Syahrasyad <nanda.s@hey.com>
2021-10-08 11:09:35 -0700

    refactor: index page to stitches

---
+++ pages/index.tsx
@@ -0,0 +1,169 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { styled } from '@/stitches'
+import { HiArrowRight } from 'react-icons/hi'
+
+import ExternalLink from '@/elements/ExternalLink'
+import { formatPath } from '@/lib/utils'
+
+import { frontMatter as slidingWindow } from './sliding-window.mdx'
+import { frontMatter as debuggerPost } from './debugger.mdx'
+
+const posts = [debuggerPost, slidingWindow]
+
+posts.sort((a, b) => (new Date(a.editedAt) < new Date(b.editedAt) ? 1 : -1))
+
+export default function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>Not a Number</title>
+      </Head>
+      <Wrapper>
+        <Header>
+          <Title>Not a Number</Title>
+          <Description>
+            An interactive blog on computer science and web development, by
+            {` `}
+            <ExternalLink href="https://narendras.io">
+              Nanda Syahrasyad
+            </ExternalLink>
+            .
+          </Description>
+        </Header>
+        <Divider />
+        <Posts>
+          {posts.map((post) => (
+            <Post key={post.__resourcePath} post={post} />
+          ))}
+        </Posts>
+      </Wrapper>
+    </>
+  )
+}
+
+const Wrapper = styled('div', {
+  paddingTop: '$32',
+  paddingBottom: '$32',
+})
+
+const Title = styled('h1', {
+  fontFamily: '$serif',
+  textAlign: 'center',
+  fontSize: '6rem',
+  fontWeight: '600',
+  lineHeight: '0.9',
+})
+
+const Header = styled('header', {
+  marginBottom: '$16',
+  textAlign: 'center',
+
+  '> :not(:last-child)': {
+    marginBottom: '$16',
+  },
+})
+
+const Description = styled('p', {
+  fontSize: '$xl',
+  paddingLeft: '$8',
+  paddingRight: '$8',
+  maxWidth: '720px',
+  margin: '0 auto',
+})
+
+const Posts = styled('ul', {
+  paddingTop: '$12',
+  display: 'grid',
+  gridTemplateColumns: '1fr min(65ch, 100%) 1fr',
+  position: 'relative',
+
+  '> *': {
+    gridColumn: 2,
+  },
+
+  '> :not(:last-child)': {
+    marginBottom: '$4',
+  },
+})
+
+const Divider = styled('div', {
+  width: '$24',
+  height: '$px',
+  background: '$grey400',
+  transform: 'translateX(calc(50vw - 50%))',
+})
+
+// --
+
+function Post({ post }: { post: typeof slidingWindow }) {
+  return (
+    <PostWrapper>
+      <Link href={formatPath(post.__resourcePath)}>
+        <Anchor>
+          <PostContent>
+            <PostTitle>{post.title}</PostTitle>
+            <PostDescription>{post.description}</PostDescription>
+            <PostUpdatedText>
+              Last updated{' '}
+              {new Intl.DateTimeFormat('en-US', {
+                month: 'long',
+                year: 'numeric',
+                day: 'numeric',
+              }).format(new Date(post.editedAt))}
+            </PostUpdatedText>
+          </PostContent>
+          <PostArrow>
+            <HiArrowRight />
+          </PostArrow>
+        </Anchor>
+      </Link>
+    </PostWrapper>
+  )
+}
+
+const PostWrapper = styled('li', {
+  padding: '$4 $8',
+  borderRadius: '12px',
+
+  '&:hover': {
+    background: '$grey200',
+  },
+})
+
+const Anchor = styled('a', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  cursor: 'pointer',
+
+  '> :not(:last-child)': {
+    marginRight: '$8',
+  },
+})
+
+const PostTitle = styled('h1', {
+  fontWeight: '500',
+  fontSize: '$2xl',
+  fontFamily: '$serif',
+})
+
+const PostDescription = styled('p', {
+  color: '$grey600',
+})
+
+const PostUpdatedText = styled('p', {
+  fontSize: '$sm',
+  color: '$grey600',
+})
+
+const PostArrow = styled('p', {
+  fontSize: '$xl',
+  color: '$grey600',
+})
+
+const PostContent = styled('div', {
+  '> :not(:last-child)': {
+    marginBottom: '$4',
+  },
+})
