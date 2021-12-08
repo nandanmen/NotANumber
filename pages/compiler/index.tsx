c6ac1db16af6e8833b1e3765a0bfa280cb1abbc1
Nanda <nanda.s@hey.com>
2021-12-07 17:16:44 -0800

    feat: add introductory list of compiler posts

--- pages/compiler/index.tsx
+++ pages/compiler/index.tsx
@@ -1,3 +1,20 @@
 export default function CompilersPage() {
-  return <h1>Hello, world!</h1>
+  return (
+    <div>
+      <h1>Rebuilding Babel</h1>
+      <p>
+        How would you rebuild Babel from scratch? In this series, I share my
+        learnings on trying to recreate the web's most popular front-end
+        transpiler.
+      </p>
+      <ol>
+        <li>Introduction</li>
+        <li>The Terrific Tokenizer</li>
+        <li>A Patient Parser and the Intricacies of console.log</li>
+        <li>Vexing Visitors and Clunky Code Generation</li>
+        <li>Escaping Errors Elegantly</li>
+        <li>Variables, Literals, and Assignments</li>
+      </ol>
+    </div>
+  )
 }
