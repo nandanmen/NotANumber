7e50aaa6e2c0987570d68208ac06a6457e0ff270
Nanda Syahrasyad <nanda.s@hey.com>
2021-12-26 12:23:42 -0800

    feat: finish up post 1 and add reveal component

--- components/Reveal.tsx
+++ components/Reveal.tsx
@@ -1,21 +1,54 @@
+import React from 'react'
 import { styled } from '@/stitches'
 
 export function Reveal({ children }) {
+  const [revealed, setRevealed] = React.useState(false)
+
   return (
     <Wrapper>
-      {children}
-      <Filter />
+      <Content revealed={revealed}>{children}</Content>
+      {!revealed && (
+        <RevealButton onClick={() => setRevealed(true)}>
+          Reveal answer
+        </RevealButton>
+      )}
     </Wrapper>
   )
 }
 
 const Wrapper = styled('div', {
   position: 'relative',
+  padding: '$8',
+  border: '3px dashed $grey300',
+  borderRadius: 8,
 })
 
-const Filter = styled('div', {
+const Content = styled('div', {
+  filter: 'blur(8px)',
+
+  '> :not(:last-child)': {
+    marginBottom: '1em',
+  },
+
+  variants: {
+    revealed: {
+      true: {
+        filter: 'blur(0px)',
+      },
+    },
+  },
+})
+
+const RevealButton = styled('button', {
+  background: '$black',
+  color: '$white',
+  fontFamily: '$serif',
+  fontSize: '$xl',
+  fontWeight: 600,
+  padding: '$2 $4',
+  borderRadius: 8,
   position: 'absolute',
-  inset: 0,
-  background: 'rgba(0,0,0,0.5)',
-  backdropFilter: 'blur(10px)',
+  top: '50%',
+  left: '50%',
+  transform: `translate(-50%, -50%)`,
 })
