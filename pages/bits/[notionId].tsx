e059a3550e14fa0f39aa3054dde321c99d9c7412
Nanda <nanda.s@hey.com>
2022-02-01 16:02:08 -0800

    feat(bits): move bits to notion as a cms

---
+++ pages/bits/[notionId].tsx
@@ -0,0 +1,44 @@
+import { GetStaticProps, GetStaticPaths, InferGetStaticPropsType } from 'next'
+import showdown from 'showdown'
+
+import { getMarkdownFromPage, Bit, getBits } from '@/lib/notion'
+import { ArticleLayout } from '@/layouts/Article'
+
+const parser = new showdown.Converter()
+
+export default function BitPage({
+  post,
+  content,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <ArticleLayout
+      title={post.title}
+      date={post.lastEditedAt}
+      content={content}
+      backTo={{ href: '/bits', name: 'Bits' }}
+    />
+  )
+}
+
+export const getStaticProps: GetStaticProps<
+  { post: Bit; content: string },
+  { notionId: string }
+> = async ({ params }) => {
+  const posts = await getBits()
+  const post = posts.find((post) => post.id === params.notionId)
+  const contentMarkdown = await getMarkdownFromPage(params.notionId)
+  return {
+    props: {
+      post,
+      content: parser.makeHtml(contentMarkdown),
+    },
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = await getBits()
+  return {
+    paths: posts.map((post) => `/bits/${post.id}`),
+    fallback: 'blocking',
+  }
+}
