f7929c269552d1ac2cfc79a74247fa76a3a3ec12
Nanda Syahrasyad <nanda.s@hey.com>
2022-07-01 11:02:51 -0700

    feat: convert to svg

--- components/magic-motion/Flip/First.tsx
+++ components/magic-motion/Flip/First.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { styled } from '@/stitches'
+import { range } from '@/lib/utils'
 
 import {
   FlipWrapper,
   FlipDisplay,
-  Square,
   FlipConsole,
   XLine,
   YLine,
@@ -11,9 +13,8 @@ import {
 } from './shared'
 
 export const First = () => {
-  const ref = React.useRef<HTMLButtonElement>()
+  const ref = React.useRef<SVGRectElement>()
   const [box, setBox] = React.useState<DOMRect>(null)
-  const [hovering, setHovering] = React.useState(false)
 
   function record() {
     setBox(ref.current.getBoundingClientRect())
@@ -21,20 +22,39 @@ export const First = () => {
 
   return (
     <FlipWrapper>
-      <FlipDisplay>
-        {(hovering || box) && (
-          <>
-            <XLine />
-            <YLine style={{ x: '2rem' }} />
-          </>
-        )}
-        <Square
-          ref={ref}
-          onHoverStart={() => setHovering(true)}
-          onHoverEnd={() => setHovering(false)}
-          onClick={record}
-        />
-      </FlipDisplay>
+      <Display>
+        <Grid>
+          <motion.g
+            style={{ x: 5, y: 20 }}
+            whileHover="hover"
+            variants={{ hover: { scale: 1.1 } }}
+          >
+            <rect
+              fill="var(--gray200)"
+              width="15"
+              height="15"
+              rx="1"
+              x="1"
+              y="1"
+            />
+            <Square
+              ref={ref}
+              fill="rgb(183, 217, 248)"
+              width="15"
+              height="15"
+              rx="1"
+              stroke="currentColor"
+              strokeWidth="0.1"
+              variants={{
+                hover: {
+                  strokeWidth: 0.3,
+                },
+              }}
+              onClick={record}
+            />
+          </motion.g>
+        </Grid>
+      </Display>
       <FlipConsole>
         {box ? (
           <DomRect label="First position" box={box} />
@@ -45,3 +65,54 @@ export const First = () => {
     </FlipWrapper>
   )
 }
+
+const Square = styled(motion.rect, {
+  cursor: 'pointer',
+})
+
+const Display = styled('div', {
+  background: '$white',
+  borderRight: '1px solid $black',
+})
+
+const CELL_SIZE = 8
+
+const Grid: React.FC<{ rows?: number; cols?: number }> = ({
+  rows = 10,
+  cols = 15,
+  children,
+}) => {
+  return (
+    <svg width="100%" viewBox="0 0 100 100" preserveAspectRatio="none">
+      <Wrapper>
+        {range(rows).map((index) => (
+          <line
+            key={`row-${index}`}
+            x1="0"
+            x2="100"
+            y1={index * CELL_SIZE}
+            y2={index * CELL_SIZE}
+            stroke="currentColor"
+            strokeWidth="0.2"
+          />
+        ))}
+        {range(cols).map((index) => (
+          <line
+            key={`col-${index}`}
+            x1={index * CELL_SIZE}
+            x2={index * CELL_SIZE}
+            y1="0"
+            y2="100"
+            stroke="currentColor"
+            strokeWidth="0.2"
+          />
+        ))}
+      </Wrapper>
+      {children}
+    </svg>
+  )
+}
+
+const Wrapper = styled('g', {
+  color: '$grey100',
+})
