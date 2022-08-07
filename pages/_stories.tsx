c1694c13050f8ab816a25523d9561eab50e0482a
Nanda <nanda.s@hey.com>
2022-08-07 14:42:52 -0700

    feat: exclude stories page from build

---
+++ pages/_stories.tsx
@@ -0,0 +1,123 @@
+import type { GetStaticProps } from "next";
+import React from "react";
+
+import { ThemeToggle } from "~/components/ThemeToggle";
+import { styled } from "~/stitches.config";
+import { stories } from "../stories.meta";
+
+export const getStaticProps: GetStaticProps = async () => {
+  if (process.env.NODE_ENV === "production") {
+    return { notFound: true };
+  }
+  return { props: {} };
+};
+
+export default function StoriesPage() {
+  const [selectedStory, setSelectedStory] = React.useState<string | null>(null);
+  const activeStory = stories.find((story) => story.name === selectedStory);
+
+  return (
+    <Main>
+      <ToggleWrapper>
+        <ThemeToggle />
+      </ToggleWrapper>
+      <Sidebar>
+        <ul>
+          {stories.map(({ name }) => (
+            <li key={name}>
+              <button onClick={() => setSelectedStory(name)}>{name}</button>
+            </li>
+          ))}
+        </ul>
+      </Sidebar>
+      <ContentWrapper>
+        {activeStory && (
+          <Content>
+            {Object.entries(activeStory.variants).map(
+              ([variant, Component]) => (
+                <>
+                  <h2>{variant}</h2>
+                  <Component />
+                </>
+              )
+            )}
+          </Content>
+        )}
+      </ContentWrapper>
+    </Main>
+  );
+}
+
+const ToggleWrapper = styled("div", {
+  position: "fixed",
+  top: "$8",
+  right: "$8",
+});
+
+const Main = styled("main", {
+  display: "grid",
+  gridTemplateColumns: "200px 1fr",
+  gap: "$8",
+  minHeight: "calc(100vh - calc($space$16 * 2))",
+});
+
+const Sidebar = styled("aside", {
+  borderRight: "1px solid $gray8",
+  fontFamily: "$mono",
+  position: "fixed",
+  top: "0",
+  bottom: "0",
+  width: 200,
+  padding: "$4",
+
+  ul: {
+    listStyle: "none",
+  },
+
+  li: {
+    cursor: "pointer",
+
+    "&:hover": {
+      backgroundColor: "$gray6",
+      borderRadius: 4,
+    },
+  },
+
+  button: {
+    padding: "$2",
+    display: "block",
+    width: "100%",
+    height: "100%",
+  },
+});
+
+const ContentWrapper = styled("div", {
+  gridColumn: 2,
+  paddingRight: "$8",
+});
+
+const Content = styled("ul", {
+  maxWidth: 800,
+  display: "grid",
+  gridTemplateColumns: "min(100%, 65ch) 1fr",
+  margin: "0 auto",
+  listStyle: "none",
+
+  "> *": {
+    gridColumn: "1",
+  },
+
+  "> .full-width": {
+    gridColumn: "1 / -1",
+  },
+
+  h2: {
+    fontFamily: "$mono",
+    color: "$gray11",
+    marginBottom: "$8",
+
+    "&:not(:first-child)": {
+      marginTop: "$16",
+    },
+  },
+});
---
+++ stories.meta.ts
@@ -0,0 +1,4 @@
+import * as LayoutExample from './_dist-content/framer-magic-motion/components/LayoutExample/LayoutExample.stories'
+import * as Tokenizer from './_dist-content/tokenizer/components/Tokenizer/Tokenizer.stories'
+export const stories = [{ name: 'LayoutExample', variants: LayoutExample, postName: 'framer-magic-motion' },
+{ name: 'Tokenizer', variants: Tokenizer, postName: 'tokenizer' }];
