c44f251e8ec83f3f60288c044082987729a84b7f
Nanda <nanda.s@hey.com>
2022-08-11 16:11:58 -0700

    feat: add simple layout example

--- content/framer-magic-motion/components/LayoutExample/LayoutExample.stories.tsx
+++ content/framer-magic-motion/components/LayoutExample/LayoutExample.stories.tsx
@@ -1,3 +1,10 @@
+import { FullWidth } from "~/components/FullWidth";
 import { LayoutExample } from "./LayoutExample";
 
 export const Default = () => <LayoutExample />;
+
+export const WithFullWidth = () => (
+  <FullWidth>
+    <LayoutExample />
+  </FullWidth>
+);
--- content/framer-magic-motion/components/LayoutExample/LayoutExample.tsx
+++ content/framer-magic-motion/components/LayoutExample/LayoutExample.tsx
@@ -1,5 +1,25 @@
+import React from "react";
+import { motion } from "framer-motion";
+
 import { GridOverflowBox } from "~/components/Grid";
+import { styled } from "~/stitches.config";
 
 export const LayoutExample = () => {
-  return <GridOverflowBox>Hello</GridOverflowBox>;
+  const [open, toggle] = React.useReducer((state) => !state, false);
+
+  return (
+    <GridOverflowBox>
+      <Wrapper>
+        <motion.div>
+          <button onClick={toggle}>Hello!</button>
+          {open && <p>I'm more content!</p>}
+        </motion.div>
+        <motion.p layout>Hello!</motion.p>
+      </Wrapper>
+    </GridOverflowBox>
+  );
 };
+
+const Wrapper = styled("div", {
+  width: 300,
+});
