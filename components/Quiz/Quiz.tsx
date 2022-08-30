0b4a70a02a63860ad7d996d35544940f291ee9ab
Nanda <nanda.s@hey.com>
2022-08-30 15:08:14 -0700

    feat: add quiz spoilers

--- components/Quiz/Quiz.tsx
+++ components/Quiz/Quiz.tsx
@@ -5,12 +5,25 @@ import { styled } from "~/stitches.config";
 
 export const QuizContext = React.createContext(null);
 
-export const Quiz = (props) => {
+export const Quiz = ({ label, answer, children }) => {
   const [selected, select] = React.useState(null);
+  const isCorrect = selected === answer;
+
+  React.useEffect(() => {
+    if (isCorrect) {
+      const spoilers = document.querySelectorAll(
+        `[data-spoiler-for="${label}"]`
+      );
+      spoilers.forEach((spoiler) =>
+        spoiler.setAttribute("data-revealed", "true")
+      );
+    }
+  }, [isCorrect, label]);
+
   return (
-    <QuizWrapper id={props.label}>
-      <QuizContext.Provider value={{ selected, select, answer: props.answer }}>
-        {props.children}
+    <QuizWrapper id={label} data-answered={isCorrect}>
+      <QuizContext.Provider value={{ selected, select, answer: answer }}>
+        {children}
       </QuizContext.Provider>
     </QuizWrapper>
   );
@@ -40,19 +53,20 @@ const OptionsWrapper = styled("div", {
   gap: "$2",
 });
 
-const Option = (props) => {
+const Option = ({ label, children }) => {
   const { selected, select } = React.useContext(QuizContext);
   return (
     <OptionWrapper
-      onClick={() => select(props.label)}
-      data-active={selected === props.label}
+      onClick={() => select(label)}
+      data-active={selected === label}
+      whileTap={{ scale: 0.95 }}
     >
-      {props.children}
+      {children}
     </OptionWrapper>
   );
 };
 
-const OptionWrapper = styled("button", {
+const OptionWrapper = styled(motion.button, {
   border: "1px solid $gray8",
   background: "$gray1",
   padding: "$1 $2",
@@ -75,13 +89,21 @@ const Tip = ({ htmlFor, children }) => {
   }
 
   const isCorrect = answer === htmlFor;
-  return <TipWrapper correct={isCorrect}>{children}</TipWrapper>;
+  return (
+    <TipWrapper
+      correct={isCorrect}
+      initial={{ opacity: 0, y: -8 }}
+      animate={{ opacity: 1, y: 0 }}
+    >
+      {children}
+    </TipWrapper>
+  );
 };
 
 const TipWrapper = styled(motion.div, {
   padding: "$2",
-  background: "$yellow4",
-  border: "1px solid $yellow7",
+  background: "$gray4",
+  border: "1px solid $gray7",
   borderRadius: 4,
 
   variants: {
@@ -94,9 +116,43 @@ const TipWrapper = styled(motion.div, {
   },
 });
 
-const Spoiler = (props) => {
-  return <span>{props.children}</span>;
+const Spoiler = ({ htmlFor, children }) => {
+  const [revealed, setRevealed] = React.useState(false);
+
+  React.useEffect(() => {
+    const quiz = document.querySelector(`#${htmlFor}[data-answered="true"]`);
+    if (quiz) {
+      setRevealed(true);
+    }
+  }, [htmlFor]);
+
+  return (
+    <SpoilerWrapper
+      onClick={() => setRevealed(true)}
+      data-revealed={revealed}
+      data-spoiler-for={htmlFor}
+    >
+      {children}
+    </SpoilerWrapper>
+  );
 };
+
+const SpoilerWrapper = styled("button", {
+  position: "relative",
+
+  "&:after": {
+    content: "",
+    position: "absolute",
+    inset: 0,
+    background: "$gray7",
+    opacity: 1,
+    transition: "opacity 0.2s ease-out",
+  },
+
+  "&[data-revealed='true']:after": {
+    opacity: 0,
+  },
+});
 
 Quiz.Question = Question;
 Quiz.Option = Option;
