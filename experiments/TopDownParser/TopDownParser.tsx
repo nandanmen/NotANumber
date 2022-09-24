ae29f17d1bda252871f643b5d0ada3bd1597a40b
Nanda Syahrasyad <nanda.s@hey.com>
2022-09-24 09:34:02 -0700

    feat: add top down parser test

--- experiments/TopDownParser/TopDownParser.tsx
+++ experiments/TopDownParser/TopDownParser.tsx
@@ -1,1 +1,68 @@
-export const TopDownParser = () => { return null; }
+import { GridBackground } from "~/components/Grid";
+
+type Token = {
+  type: string;
+  value: string;
+};
+
+type Group = {
+  type: string;
+  children: (Group | Token)[];
+};
+
+const token = (value: string) => ({ type: "token", value });
+
+const keyword = (value: string) => ({ type: "keyword", value });
+
+const oneOf = (tokens: (Group | Token)[]) => ({
+  type: "or",
+  children: tokens,
+});
+
+const optionalMany = (tokens: (Group | Token)[]) => ({
+  type: "optionalMany",
+  children: tokens,
+});
+
+const rule = (rule: string) => ({
+  type: "rule",
+  value: rule,
+});
+
+const RULES = {
+  VariableDeclaration: [
+    oneOf([keyword("const"), keyword("let"), keyword("var")]),
+    token("="),
+    rule("Expression"),
+  ],
+  Expression: [
+    oneOf([
+      token("Identifier"),
+      rule("MemberExpression"),
+      rule("CallExpression"),
+    ]),
+  ],
+  MemberExpression: [
+    token("Identifier"),
+    optionalMany([token("."), rule("Expression")]),
+  ],
+  CallExpression: [
+    token("Identifier"),
+    optionalMany([token("("), optionalMany([token("Identifier")]), token(")")]),
+  ],
+};
+
+export const TopDownParser = () => {
+  return (
+    <GridBackground>
+      {Object.entries(RULES).map(([name, definition]) => {
+        console.log(definition);
+        return (
+          <div key={name}>
+            <p>{name}</p>
+          </div>
+        );
+      })}
+    </GridBackground>
+  );
+};
--- stories.meta.ts
+++ stories.meta.ts
@@ -1,4 +1,6 @@
 import * as LayoutExample from './_dist-content/framer-magic-motion/components/LayoutExample/LayoutExample.stories'
 import * as Tokenizer from './_dist-content/tokenizer/components/Tokenizer/Tokenizer.stories'
+import * as TopDownParser from './experiments/TopDownParser/TopDownParser.stories'
 export const stories = [{ name: `framer-magic-motion`, stories: [{ name: 'LayoutExample', variants: LayoutExample }] },
-{ name: `tokenizer`, stories: [{ name: 'Tokenizer', variants: Tokenizer }] }];
+{ name: `tokenizer`, stories: [{ name: 'Tokenizer', variants: Tokenizer }] },
+{ name: `experiments`, stories: [{ name: 'TopDownParser', variants: TopDownParser }] }];
