7aac3aa8740c18f2b692a9f716c29a0e03a8ea7c
Nanda <nanda.s@hey.com>
2022-10-01 18:56:01 -0700

    refactor: abstract out size diagram

---
+++ content/magic-motion/components/shared/SizeDiagram/SizeDiagram.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { motion, MotionValue } from "framer-motion";
+
+import { styled } from "~/stitches.config";
+
+const PADDING = 45;
+const SQUARE_RADIUS = 60;
+
+export type SizeDiagramProps = {
+  scale: MotionValue<number>;
+  onWidthChange?: (width: number) => void;
+};
+
+export const SizeDiagram = ({ scale, onWidthChange }: SizeDiagramProps) => {
+  const [width, setWidth] = React.useState(SQUARE_RADIUS * 2);
+
+  const svgRef = React.useRef<SVGSVGElement>();
+  const finalRef = React.useRef<SVGRectElement>();
+
+  React.useEffect(() => {
+    const { width } = svgRef.current.getBoundingClientRect();
+    setWidth(width - PADDING * 2);
+  }, []);
+
+  const lineRef = React.useRef<SVGLineElement>();
+  const textRef = React.useRef<SVGTextElement>();
+
+  const updateRefs = React.useCallback(
+    (value: number) => {
+      finalRef.current?.setAttribute("width", (value * width).toString());
+      lineRef.current?.setAttribute("x2", (value * width - 2).toString());
+      textRef.current?.setAttribute("x", ((width * value) / 2).toString());
+      if (textRef.current) {
+        textRef.current.textContent = `scale(${value.toFixed(2)})`;
+      }
+    },
+    [width]
+  );
+
+  React.useEffect(() => {
+    if (onWidthChange) {
+      onWidthChange(width);
+    }
+  }, [width, onWidthChange]);
+
+  React.useEffect(() => {
+    updateRefs(scale.get());
+    return scale.onChange(updateRefs);
+  }, [updateRefs, scale]);
+
+  return (
+    <svg ref={svgRef} width="100%" height="100%">
+      <OriginalSquareWrapper width={width}>
+        <OriginalSquare />
+      </OriginalSquareWrapper>
+      <Square ref={finalRef} x={PADDING} />
+      <motion.g style={{ x: PADDING, y: `calc(50% - ${SQUARE_RADIUS}px)` }}>
+        <AnchorLine ref={lineRef} x1="2" y1="2" y2="118" />
+        <TranslateText ref={textRef} y={64} textAnchor="middle" />
+      </motion.g>
+    </svg>
+  );
+};
+
+const OriginalSquareWrapper = styled("foreignObject", {
+  height: 120,
+  rx: "6px",
+  y: `calc(50% - ${SQUARE_RADIUS}px)`,
+  filter: "drop-shadow(var(--shadows-sm))",
+  x: PADDING,
+});
+
+const OriginalSquare = styled("div", {
+  background: `repeating-linear-gradient( -45deg, $colors$gray8, $colors$gray8 5px, transparent 5px, transparent 10px )`,
+  border: "1px solid $gray8",
+  height: 120,
+  width: "100%",
+  borderRadius: "$base",
+});
+
+const TranslateText = styled(motion.text, {
+  fontFamily: "$mono",
+  fontSize: "$sm",
+  fill: "$blue11",
+});
+
+const AnchorLine = styled("line", {
+  stroke: "$blue7",
+  strokeWidth: 1,
+  strokeDasharray: "6",
+});
+
+const Square = styled(motion.rect, {
+  height: 120,
+  rx: "6px",
+  y: `calc(50% - ${SQUARE_RADIUS}px)`,
+  filter: "drop-shadow(var(--shadows-sm))",
+  fill: "$blue5",
+  stroke: "$blue7",
+});
