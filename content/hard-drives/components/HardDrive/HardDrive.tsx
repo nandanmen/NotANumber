47b9248a07e1db46cd8858530407711d9ee44f59
Nanda Syahrasyad <nanda.s@hey.com>
2022-11-20 09:41:37 -0800

    feat: add active sector implementation

--- content/hard-drives/components/HardDrive/HardDrive.tsx
+++ content/hard-drives/components/HardDrive/HardDrive.tsx
@@ -1,7 +1,13 @@
+import React from "react";
 import { motion } from "framer-motion";
-import { FullWidth } from "~/components/FullWidth";
 
-import { Visualizer, Content } from "~/components/Visualizer";
+import { FullWidth } from "~/components/FullWidth";
+import {
+  Visualizer,
+  Content,
+  Controls,
+  ToggleButton,
+} from "~/components/Visualizer";
 import { styled } from "~/stitches.config";
 
 const X_MAX = 120;
@@ -26,6 +32,8 @@ const range = (from: number, to: number, skip = 1) => {
 };
 
 export const HardDrive = () => {
+  const [spinning, setSpinning] = React.useState(false);
+
   return (
     <FullWidth>
       <Visualizer>
@@ -44,7 +52,7 @@ export const HardDrive = () => {
                 type: "tween",
                 duration: 5,
                 ease: "linear",
-                repeat: Number.POSITIVE_INFINITY,
+                repeat: spinning ? Number.POSITIVE_INFINITY : 0,
               }}
             >
               <Disk r={DISK_RADIUS} />
@@ -78,14 +86,55 @@ export const HardDrive = () => {
                     to={38}
                   />
                 ))}
+                <ActiveSector
+                  startAngle={0}
+                  endAngle={20}
+                  startRadius={20.5}
+                  endRadius={29.5}
+                />
+                <ActiveSector
+                  startAngle={120}
+                  endAngle={150}
+                  startRadius={12}
+                  endRadius={20.5}
+                />
               </g>
             </motion.g>
           </svg>
         </ContentWrapper>
+        <Controls>
+          <ToggleButton onClick={() => setSpinning(!spinning)}>
+            Toggle
+          </ToggleButton>
+        </Controls>
       </Visualizer>
     </FullWidth>
   );
 };
+
+const ActiveSector = ({ startAngle, endAngle, startRadius, endRadius }) => {
+  const lowerArcStart = getPointAtAngle(startAngle, startRadius);
+  const lowerArcEnd = getPointAtAngle(endAngle, startRadius);
+  const upperArcStart = getPointAtAngle(startAngle, endRadius);
+  const upperArcEnd = getPointAtAngle(endAngle, endRadius);
+
+  const d = `
+    M ${lowerArcStart.x} ${lowerArcStart.y}
+    A ${startRadius} ${startRadius} 0 0 1 ${lowerArcEnd.x} ${lowerArcEnd.y}
+    L ${upperArcEnd.x} ${upperArcEnd.y}
+    A ${endRadius} ${endRadius} 0 0 0 ${upperArcStart.x} ${upperArcStart.y}
+    Z
+  `;
+
+  return <_ActiveSector d={d} />;
+};
+
+const _ActiveSector = styled("path", {
+  fill: "$blue6",
+  stroke: "$blue8",
+  strokeWidth: "0.2",
+  filter: "drop-shadow($shadows$md)",
+});
 
 const Sector = ({ angle, from, to }) => {
   const { x: x1, y: y1 } = getPointAtAngle(angle, from);
@@ -99,14 +148,16 @@ const ContentWrapper = styled(Content, {
 
 const SectorLine = styled("line", {
   fill: "none",
-  stroke: "$gray8",
+  stroke: "$gray9",
   strokeWidth: "0.2",
+  strokeDasharray: "1.2",
 });
 
 const Track = styled("circle", {
   fill: "none",
-  stroke: "$gray8",
+  stroke: "$gray9",
   strokeWidth: "0.2",
+  strokeDasharray: "1.2",
 });
 
 const Disk = styled("circle", {
