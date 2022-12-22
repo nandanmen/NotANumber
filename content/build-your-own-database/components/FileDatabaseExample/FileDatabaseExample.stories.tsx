7b37a5f2db75e7f1cf2feba5c4b211b42ef02684
Nanda Syahrasyad <nanda.s@hey.com>
2022-12-22 12:20:19 -0800

    fix: ts error in stories

--- content/build-your-own-database/components/FileDatabaseExample/FileDatabaseExample.stories.tsx
+++ content/build-your-own-database/components/FileDatabaseExample/FileDatabaseExample.stories.tsx
@@ -1,3 +1,22 @@
-import { FileDatabaseExample } from './FileDatabaseExample'
+import { type Record } from "../FileDatabase";
+import { FileDatabaseExample } from "./FileDatabaseExample";
 
-export const Default = () => <FileDatabaseExample />
+const records: Record[] = [
+  {
+    value: [3, "Lorem ipsum"],
+  },
+  {
+    value: [1, "Lorem ipsum"],
+    type: "success",
+  },
+  {
+    value: [18, "Lorem ipsum"],
+    type: "active",
+  },
+];
+
+export const Default = () => <FileDatabaseExample records={records} />;
+
+export const FullWidth = () => (
+  <FileDatabaseExample records={records} fullWidth />
+);
