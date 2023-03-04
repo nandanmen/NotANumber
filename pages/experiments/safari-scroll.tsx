5fcdbae7de09321a9f2fa7a8f47127a9c8360747
Nanda Syahrasyad <nanda.s@hey.com>
2023-03-04 10:30:09 -0800

    feat: implement safari scroll

--- pages/experiments/safari-scroll.tsx
+++ pages/experiments/safari-scroll.tsx
@@ -1,17 +1,69 @@
 import React from "react";
 import { ExperimentsPage } from "~/components/layout/ExperimentsPage";
 import { GoTextSize } from "react-icons/go";
-import { motion } from "framer-motion";
+import {
+  motion,
+  MotionValue,
+  transform,
+  useMotionValue,
+  useSpring,
+  useTransform,
+} from "framer-motion";
 import { FiChevronLeft, FiChevronRight, FiShare, FiCopy } from "react-icons/fi";
 import { HiOutlineBookOpen } from "react-icons/hi";
 import { FaUndo } from "react-icons/fa";
 import { styled } from "~/stitches.config";
 
+function useScrollOffset(ref: React.RefObject<HTMLElement>) {
+  const lastScrollTop = React.useRef(0);
+  const scrollOffset = useMotionValue(0);
+
+  React.useEffect(() => {
+    if (!ref.current) return;
+    const wrapper = ref.current;
+
+    let timeout = null;
+    const handleScroll = () => {
+      clearTimeout(timeout);
+
+      const scrollTop = wrapper.scrollTop;
+      const offset = scrollTop - lastScrollTop.current;
+      lastScrollTop.current = scrollTop;
+      scrollOffset.set(scrollOffset.get() + offset);
+
+      timeout = setTimeout(() => {
+        scrollOffset.set(0);
+      }, 400);
+    };
+
+    wrapper.addEventListener("scroll", handleScroll);
+    return () => {
+      wrapper.removeEventListener("scroll", handleScroll);
+      clearTimeout(timeout);
+    };
+  }, [ref, scrollOffset]);
+
+  return scrollOffset;
+}
+
+const MAX_OFFSET = 50;
+
 export default function SafariScrollPage() {
+  const wrapperRef = React.useRef<HTMLDivElement>();
   const [open, setOpen] = React.useState(true);
+  const scrollOffset = useScrollOffset(wrapperRef);
+
+  React.useEffect(() => {
+    scrollOffset.on("change", (v) => {
+      if (v > MAX_OFFSET) setOpen(false);
+      if (v < -MAX_OFFSET) setOpen(true);
+    });
+  }, [scrollOffset]);
+
   return (
     <ExperimentsPage page="safari-scroll">
       <Box
+        as={motion.div}
         css={{
           width: 500,
           margin: "0 auto",
@@ -23,6 +75,7 @@ export default function SafariScrollPage() {
           fontSize: "$lg",
           overflowY: "auto",
         }}
+        ref={wrapperRef}
       >
         <Box>
           <Article />
@@ -36,84 +89,102 @@ export default function SafariScrollPage() {
             overflow: "hidden",
           }}
         >
-          <Box
-            css={{
-              background: "$gray2",
-              padding: "$6",
-              paddingTop: "$4",
-              paddingBottom: "$10",
-              borderBottomLeftRadius: 16,
-              borderBottomRightRadius: 16,
-              borderTop: "1px solid $gray3",
-            }}
-            animate={{ y: open ? 0 : 80 }}
-          >
-            <Box
-              css={{
-                display: "flex",
-                alignItems: "center",
-                background: "var(--background)",
-                borderRadius: 8,
-                padding: "$2 $4",
-                boxShadow:
-                  "0 25px 50px -12px rgb(0 0 0 / var(--shadow-opacity))",
-              }}
-              animate={
-                {
-                  "--shadow-opacity": open ? 0.25 : 0,
-                  "--background": open
-                    ? "var(--colors-gray3)"
-                    : "var(--colors-gray2)",
-                } as any
-              }
-            >
-              <Icon animate={{ opacity: open ? 1 : 0 }}>
-                <GoTextSize />
-              </Icon>
-              <Box
-                css={{ width: "100%", textAlign: "center" }}
-                animate={{ fontSize: open ? "1em" : "0.75em" }}
-              >
-                nan.fyi
-              </Box>
-              <Icon animate={{ opacity: open ? 1 : 0 }}>
-                <FaUndo size="0.9em" />
-              </Icon>
-            </Box>
-            <Box
-              css={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                fontSize: "$xl",
-                marginTop: "$6",
-              }}
-            >
-              <Icon>
-                <FiChevronLeft />
-              </Icon>
-              <Icon>
-                <FiChevronRight />
-              </Icon>
-              <Icon>
-                <FiShare />
-              </Icon>
-              <Icon>
-                <HiOutlineBookOpen size="1.2em" />
-              </Icon>
-              <Icon>
-                <FiCopy />
-              </Icon>
-            </Box>
-          </Box>
+          <Navbar open={open} scrollOffset={scrollOffset} />
         </Box>
-      </Box>
-      <Box css={{ width: "fit-content", margin: "0 auto", marginTop: "$6" }}>
-        <button onClick={() => setOpen(!open)}>Toggle</button>
       </Box>
     </ExperimentsPage>
   );
 }
+
+const Navbar = ({
+  open,
+  scrollOffset,
+}: {
+  open: boolean;
+  scrollOffset: MotionValue<number>;
+}) => {
+  const yOffset = useTransform(scrollOffset, (offset) => {
+    if (open) {
+      return transform(offset, [0, MAX_OFFSET], [0, 20]);
+    }
+    return transform(offset, [-MAX_OFFSET, 0], [60, 80]);
+  });
+  const y = useSpring(yOffset, {
+    damping: 30,
+    stiffness: 300,
+  });
+  return (
+    <Box
+      css={{
+        background: "$gray2",
+        padding: "$6",
+        paddingTop: "$4",
+        paddingBottom: "$10",
+        borderBottomLeftRadius: 16,
+        borderBottomRightRadius: 16,
+        borderTop: "1px solid $gray3",
+      }}
+      style={{ y }}
+    >
+      <Box
+        css={{
+          display: "flex",
+          alignItems: "center",
+          background: "var(--background)",
+          borderRadius: 8,
+          padding: "$2 $4",
+          boxShadow: "0 25px 50px -12px rgb(0 0 0 / var(--shadow-opacity))",
+        }}
+        animate={
+          {
+            "--shadow-opacity": open ? 0.25 : 0,
+            "--background": open
+              ? "var(--colors-gray3)"
+              : "var(--colors-gray2)",
+          } as any
+        }
+      >
+        <Icon animate={{ opacity: open ? 1 : 0 }}>
+          <GoTextSize />
+        </Icon>
+        <Box
+          css={{ width: "100%", textAlign: "center" }}
+          animate={{ fontSize: open ? "1em" : "0.75em" }}
+        >
+          nan.fyi
+        </Box>
+        <Icon animate={{ opacity: open ? 1 : 0 }}>
+          <FaUndo size="0.9em" />
+        </Icon>
+      </Box>
+      <Box
+        css={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          fontSize: "$xl",
+          marginTop: "$6",
+        }}
+      >
+        <Icon>
+          <FiChevronLeft />
+        </Icon>
+        <Icon>
+          <FiChevronRight />
+        </Icon>
+        <Icon>
+          <FiShare />
+        </Icon>
+        <Icon>
+          <HiOutlineBookOpen size="1.2em" />
+        </Icon>
+        <Icon>
+          <FiCopy />
+        </Icon>
+      </Box>
+    </Box>
+  );
+};
 
 const Article = () => {
   return (
