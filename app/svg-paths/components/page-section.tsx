cb6586bf9910171053b414a0ed5e95b0f7013b51
Nanda Syahrasyad <nanda.s@hey.com>
2023-04-22 09:39:48 -0700

    fix: small layout fixes

--- app/svg-paths/components/page-section.tsx
+++ app/svg-paths/components/page-section.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useIndexContext } from "./index-provider";
 
+const isVisible = (element: HTMLElement) => {
+  const { top } = element.getBoundingClientRect();
+  return top < window.innerHeight / 2;
+};
+
 export function PageSection({
   index,
   children,
@@ -12,12 +17,15 @@ export function PageSection({
   const { set } = useIndexContext();
 
   React.useEffect(() => {
+    if (ref.current && isVisible(ref.current)) {
+      set(index);
+    }
+  }, [index, set]);
+
+  React.useEffect(() => {
     const handleScroll = () => {
-      if (ref.current) {
-        const { top } = ref.current.getBoundingClientRect();
-        if (top < window.innerHeight / 2) {
-          set(index);
-        }
+      if (ref.current && isVisible(ref.current)) {
+        set(index);
       }
     };
     window.addEventListener("scroll", handleScroll);
@@ -29,7 +37,7 @@ export function PageSection({
   return (
     <section
       ref={ref}
-      className="p-10 pb-32 min-h-[50vh] space-y-[1.5em] relative before:h-px before:border-gray8 before:border-t before:border-dashed before:absolute before:right-0 before:top-0 before:w-1/4 first-of-type:before:hidden"
+      className="p-16 pb-32 min-h-[50vh] space-y-[1.5em] relative before:h-px before:border-gray8 before:border-t before:border-dashed before:absolute before:right-0 before:top-0 before:w-1/4 first-of-type:before:hidden"
     >
       {children}
     </section>
--- app/svg-paths/layout.tsx
+++ app/svg-paths/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-gray4">
-      <main className="grid grid-cols-[60ch_1fr] min-h-100vh">{children}</main>
+      <main className="grid grid-cols-[68ch_1fr] min-h-100vh">{children}</main>
     </div>
   );
 }
