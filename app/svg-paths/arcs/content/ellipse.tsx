8e37bd47e365349f0de29c089b73adfa38f633d0
Nanda Syahrasyad <nanda.s@hey.com>
2023-05-27 09:42:05 -0700

    feat: create ellipse visual

--- app/svg-paths/arcs/content/ellipse.tsx
+++ app/svg-paths/arcs/content/ellipse.tsx
@@ -1,4 +1,10 @@
 import { useStateContext } from "app/svg-paths/components/state-context";
+import { useSvgContext } from "app/svg-paths/components/svg";
+import {
+  CoordinatesTooltip,
+  Tooltip,
+} from "app/svg-paths/components/svg/tooltip";
+import { getArcCenter } from "app/svg-paths/components/utils";
 import { parsePath } from "app/svg-paths/lib/path";
 import { ArcSandbox } from "./arc-sandbox";
 import { createInitialState } from "./drag-group";
@@ -13,8 +19,24 @@ export const initialState = {
 };
 
 function Ellipse() {
+  const { getRelative } = useSvgContext();
   const { data, set } = useStateContext<SyntaxState>("ellipse");
-  return <ArcSandbox {...data} set={set} />;
+  const { path } = data;
+  const arc = path.atAbsolute<"A">(1);
+  const { cx, cy } = getArcCenter(arc);
+  return (
+    <>
+      <ArcSandbox {...data} set={set} />
+      <CoordinatesTooltip x={arc.x0} y={arc.y0} placement="top" />
+      <CoordinatesTooltip x={arc.x} y={arc.y} placement="bottom" />
+      <Tooltip x={cx - getRelative(1)} y={cy + arc.ry / 2} placement="left">
+        {arc.ry.toFixed(1)}
+      </Tooltip>
+      <Tooltip y={cy - getRelative(1)} x={cx + arc.rx / 2} placement="top">
+        {arc.rx.toFixed(1)}
+      </Tooltip>
+    </>
+  );
 }
 
 export const page = {
