d482e8e5099d62da87a130fe964aa9a3c7c83878
Nanda Syahrasyad <nanda.s@hey.com>
2023-06-24 23:15:33 -0700

    refactor: curve general to new path

--- app/svg-paths/cubic-curves/content/curve-general.tsx
+++ app/svg-paths/cubic-curves/content/curve-general.tsx
@@ -1,42 +1,40 @@
-import React from "react";
-import type { PointsGroup } from "app/svg-paths/components/drag-group";
-import { useSvgContext } from "app/svg-paths/components/svg";
 import { CubicPlayground } from "./cubic-playground";
+import { useStateContext } from "../state";
+import { Path } from "app/svg-paths/components/svg/path";
+import { Circle } from "app/svg-paths/components/svg/circle";
+import { Line } from "app/svg-paths/components/svg/line";
 
 function CurveGeneral() {
-  const { getRelative } = useSvgContext();
-  const [points, set] = React.useState<PointsGroup>([
-    [0, 5],
-    [20, 5],
-    [15, 13],
-  ]);
+  const { data, set } = useStateContext("syntax");
   return (
     <g>
       <g className="text-gray8">
-        <path
-          d="M 5 13 Q 10 5 15 13"
-          fill="none"
-          strokeWidth={getRelative(1.25)}
-          stroke="currentColor"
+        <Path d="M 5 13 Q 10 5 15 13" />
+        <Line dashed className="stroke-current" x1="5" y1="13" x2="10" y2="5" />
+        <Line
+          dashed
+          className="stroke-current"
+          x1="15"
+          y1="13"
+          x2="10"
+          y2="5"
         />
-        <g
-          strokeWidth={getRelative(0.75)}
-          stroke="currentColor"
-          strokeDasharray={`${getRelative(1.5)} ${getRelative(1)}`}
-        >
-          <line x1="5" y1="13" x2="10" y2="5" />
-          <line x1="15" y1="13" x2="10" y2="5" />
-        </g>
-        <circle
+        <Circle
           className="fill-gray6"
-          r={getRelative(1)}
           cx="10"
           cy="5"
           stroke="currentColor"
-          strokeWidth={getRelative(0.5)}
+          variant="point"
         />
       </g>
-      <CubicPlayground points={points} set={({ points }) => set(points)} />
+      <CubicPlayground
+        curve={data.path.atAbsolute(1)}
+        onChange={({ curve }) => {
+          set({
+            path: data.path.setAbsolute(1, curve),
+          });
+        }}
+      />
     </g>
   );
 }
