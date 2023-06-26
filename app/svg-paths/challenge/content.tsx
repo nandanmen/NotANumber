143324f9fab6dd7c82fd8c8bc3eb9357d6223c04
Nanda Syahrasyad <nanda.s@hey.com>
2023-06-25 23:09:30 -0700

    feat: center icons in bounding box

--- app/svg-paths/challenge/content.tsx
+++ app/svg-paths/challenge/content.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { motion } from "framer-motion";
 import { Button } from "../components/button";
 import { CommandListFromSource } from "../components/command-list";
 import { MDX } from "../components/mdx";
@@ -13,39 +14,58 @@ import {
 import { StateProvider, useStateContext } from "../components/state-context";
 import { VisualWrapper } from "../components/visual-wrapper";
 import { parsePath } from "../lib/path";
+import { Path } from "../components/svg/path";
+import { Circle } from "../components/svg/circle";
+import { Svg, useSvgContext } from "../components/svg";
 
 const questions = [
   {
     path: "M2.5 2.49982L9 20.4998L12.5 12.4998L20.5 8.99982L2.5 2.49982Z",
     name: "cursor",
+    x: 2,
+    y: 2,
   },
   {
     path: "M17 5L21 9L17 13M21 9H3V18H12",
     name: "arrow",
+    x: 1,
+    y: 0,
   },
   {
     path: "M 10.5 3.1 A 9 9 0 1 0 20.9 13.4 A 8 8 0 0 1 10.5 3.1 Z",
     name: "moon",
+    x: 0.5,
+    y: 0.5,
   },
   {
     path: "M 2 13 h 20 C 21 1 3 1 2 13 Z M 12 2 V 4 M 12 14 v 4 a 2 2 0 0 1 -2 2 a 2 2 0 0 1 -2 -2",
     name: "umbrella",
+    x: 1,
+    y: 1,
   },
   {
     path: "M14 16.0341C13.3267 17.8 11.7891 19.0341 10 19.0341C8.21087 19.0341 6.67327 17.8 6 16.0341M18 16.0341L16.793 6.98149C16.339 3.57682 13.4348 1.03412 10 1.03412C6.56521 1.03412 3.66097 3.57682 3.20702 6.98148L2 16.0341H18Z",
     name: "bell",
+    x: 2.5,
+    y: 2.5,
   },
   {
     path: "M11.9695 7.40912V11.4091L14.4695 13.9091M1.96948 4.65912L4.96948 1.65912M21.9695 4.65912L18.9695 1.65912 M21 11.4 a 9 9 0 0 0 -18 0 a 9 9 0 0 0 18 0 z",
     name: "clock",
+    x: 1,
+    y: 1,
   },
   {
     path: "M17.9695 4.7052L9.96948 1.9552L1.96948 4.7052V10.8675C1.96948 15.84 5.96948 17.9552 9.96948 20.1131C13.9695 17.9552 17.9695 15.84 17.9695 10.8675V4.7052Z",
     name: "shield",
+    x: 2.5,
+    y: 1.5,
   },
   {
     path: "M 2 8 C 7 -1.2 17 -1.2 22 8 C 17 17.2 7 17.2 2 8 Z M 9 8 a 3 3 0 0 0 6 0 a 3 3 0 0 0 -6 0 z",
     name: "eye",
+    x: 1,
+    y: 4.5,
   },
 ];
 
@@ -64,11 +84,15 @@ export function Content({ content, length }) {
       <MDX content={content} numSections={length} components={{ Editor }}>
         <VisualWrapper
           components={[
-            null,
-            null,
-            ...questions.map(({ name }) => {
+            { children: <PathAnimation /> },
+            { children: <IconList />, svg: false },
+            ...questions.map(({ name, x, y }) => {
               return {
-                children: <PracticeQuestion questionKey={name} />,
+                children: (
+                  <g transform={`translate(${x}, ${y})`}>
+                    <PracticeQuestion questionKey={name} />
+                  </g>
+                ),
               };
             }),
           ]}
@@ -78,18 +102,87 @@ export function Content({ content, length }) {
   );
 }
 
+function IconList() {
+  return (
+    <>
+      <Svg />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 grid grid-cols-[repeat(4,200px)] gap-8">
+        {questions.map(({ path, name }) => {
+          return (
+            <div key={name}>
+              <svg width="200" viewBox="0 0 25 25">
+                <path
+                  d={path}
+                  fill="none"
+                  strokeWidth="1"
+                  stroke="currentColor"
+                />
+              </svg>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+function PathAnimation() {
+  const { getRelative } = useSvgContext();
+  return (
+    <g>
+      <rect
+        x="5"
+        y="5"
+        width="15"
+        height="15"
+        strokeWidth={getRelative(1)}
+        className="fill-none stroke-gray8"
+      />
+      <Path
+        d="M 5 5 q 7.5 0 15 0 q 0 7.5 0 15 q -7.5 0 -15 0 q 0 -7.5 0 -15 z"
+        animate={{
+          d: "M 12.5 5 q 7.5 0 7.5 7.5 q 0 7.5 -7.5 7.5 q -7.5 0 -7.5 -7.5 q 0 -7.5 7.5 -7.5 z",
+        }}
+        transition={{ type: "spring", duration: 2, bounce: 0.3 }}
+      />
+      <motion.g
+        animate={{ x: 7.5, y: 0 }}
+        transition={{ type: "spring", duration: 2, bounce: 0.3 }}
+      >
+        <Circle cx={5} cy={5} variant="point" />
+        <Circle cx={12.5} cy={5} variant="point" />
+      </motion.g>
+      <motion.g
+        animate={{ x: 0, y: 7.5 }}
+        transition={{ type: "spring", duration: 2, bounce: 0.3 }}
+      >
+        <Circle cx={20} cy={5} variant="point" />
+        <Circle cx={20} cy={12.5} variant="point" />
+      </motion.g>
+      <motion.g
+        animate={{ x: -7.5, y: 0 }}
+        transition={{ type: "spring", duration: 2, bounce: 0.3 }}
+      >
+        <Circle cx={20} cy={20} variant="point" />
+        <Circle cx={12.5} cy={20} variant="point" />
+      </motion.g>
+      <motion.g
+        animate={{ x: 0, y: -7.5 }}
+        transition={{ type: "spring", duration: 2, bounce: 0.3 }}
+      >
+        <Circle cx={5} cy={20} variant="point" />
+        <Circle cx={5} cy={12.5} variant="point" />
+      </motion.g>
+    </g>
+  );
+}
+
 function Editor({ questionKey }: { questionKey: string }) {
   const {
     data: { showAnswer },
     set,
   } = useStateContext<Record<string, PracticeQuestionState>>()(questionKey);
   const [showReference, setShowReference] = React.useState(true);
-
-  React.useEffect(() => {
-    if (showAnswer) {
-      setShowReference(false);
-    }
-  }, [showAnswer]);
 
   return (
     <div className="space-y-4">
