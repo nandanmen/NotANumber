79aee1a86d2a2bc3021deabca213bba620e1d6e7
Nanda Syahrasyad <nanda.s@hey.com>
2025-08-15 18:35:51 -0700

    footer

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,44 +1,95 @@
 import { BackgroundStripes } from "~/components/stripe-pattern";
 import { Metadata } from "next";
+import { posts, XIcon } from "./posts";
+import Link from "next/link";
 
 export const metadata: Metadata = {
+  title: "Not a Number",
+  description:
+    "An interactive blog on computer science and web development, by Nanda Syahrasyad.",
+  authors: [
+    {
+      name: "Nanda Syahrasyad",
+      url: "https://twitter.com/nandafyi",
+    },
+  ],
+  twitter: {
+    card: "summary_large_image",
     title: "Not a Number",
     description:
-        "An interactive blog on computer science and web development, by Nanda Syahrasyad.",
-    authors: [
-        {
-            name: "Nanda Syahrasyad",
-            url: "https://twitter.com/nandafyi",
-        },
-    ],
-    twitter: {
-        card: "summary_large_image",
-        title: "Not a Number",
-        description:
-            "An interactive blog on computer science and web development, by Nanda Syahrasyad.",
-        creator: "@nandafyi",
-    },
-    openGraph: {
-        title: "Not a Number",
-        description:
-            "An interactive blog on computer science and web development, by Nanda Syahrasyad.",
-        url: "https://nan.fyi",
-        siteName: "Not a Number",
-    },
-    metadataBase: new URL("https://nan.fyi"),
+      "An interactive blog on computer science and web development, by Nanda Syahrasyad.",
+    creator: "@nandafyi",
+  },
+  openGraph: {
+    title: "Not a Number",
+    description:
+      "An interactive blog on computer science and web development, by Nanda Syahrasyad.",
+    url: "https://nan.fyi",
+    siteName: "Not a Number",
+  },
+  metadataBase: new URL("https://nan.fyi"),
 };
 
-export default function MainLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="min-h-screen flex isolate">
-            <BackgroundStripes className="fixed inset-0" />
-            <div
-                className="relative mx-auto px-4 max-w-[1450px] w-full"
-            >
-                <div className="bg-gray4 border-x border-gray8">
-                    {children}
-                </div>
-            </div>
+export default function MainLayout({
+  children,
+}: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen isolate pb-12">
+      <BackgroundStripes className="fixed inset-0" />
+      <div className="relative z-10 mx-auto px-4 max-w-[1450px] w-full">
+        <div className="bg-gray4 border-x border-b border-gray8 pb-8 relative">
+          {children}
+          <span className="text-gray10">
+            <span className="bottom-0 right-0 absolute translate-x-1/2 translate-y-1/2">
+              <XIcon />
+            </span>
+            <span className="bottom-0 left-0 absolute -translate-x-1/2 translate-y-1/2">
+              <XIcon />
+            </span>
+          </span>
         </div>
-    )
+      </div>
+      <footer className="relative mx-auto px-4 max-w-[1250px] w-full">
+        <p className="font-serif text-[190px] translate-y-[0.15em] leading-none text-gray6 text-center">
+          Not a Number
+        </p>
+        <ul className="border border-gray8 bg-gray4 -mt-8 relative grid grid-cols-2">
+          {posts.map((post) => (
+            <li
+              className="grid grid-cols-[60px_1fr] p-4 gap-5 items-center [&:nth-child(2n)]:border-l border-gray7 border-b -mb-px"
+              key={post.slug}
+            >
+              <img
+                className="shrink-0 h-min"
+                src={post.image.src}
+                width="60"
+                height="60"
+                alt=""
+              />
+              <article>
+                <Link className="font-medium" href={post.slug}>
+                  {post.title}
+                </Link>
+                <p className="text-gray11">{post.shortDescription}</p>
+              </article>
+            </li>
+          ))}
+          <span className="text-gray10">
+            <span className="top-0 left-0 absolute -translate-x-1/2 -translate-y-1/2">
+              <XIcon />
+            </span>
+            <span className="absolute -translate-x-1/2 -translate-y-1/2">
+              <XIcon />
+            </span>
+            <span className="top-0 right-0 absolute translate-x-1/2 -translate-y-1/2">
+              <XIcon />
+            </span>
+            <span className="right-0 absolute translate-x-1/2 -translate-y-1/2">
+              <XIcon />
+            </span>
+          </span>
+        </ul>
+      </footer>
+    </div>
+  );
 }
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -6,82 +6,8 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 import clsx from "clsx";
 import { FaGithub, FaTwitter } from "react-icons/fa";
-
-import array from "../_images/array.png";
-import debuggerImg from "../_images/debugger.png";
-import slidingWindow from "../_images/sliding-window.png";
-import tokenizer from "../_images/tokenizer.png";
-import framerMagicMotion from "../_images/framer-magic-motion.png";
-import framerMotionKeys from "../_images/framer-motion-keys.png";
-import svgPaths from "../_images/svg-paths.png";
-import db from "../_images/db.png";
 import { SubscribeButton } from "./subscribe";
-
-const posts = [
-  {
-    slug: "database",
-    title: "Build Your Own Database",
-    description:
-      "How would you reinvent the database? In this post, we'll build a key-value database from the ground up.",
-    editedAt: "2025-01-03",
-    image: db,
-  },
-  {
-    slug: "svg-paths",
-    title: "A Deep Dive Into SVG Path Commands",
-    description:
-      "The mystical d attribute in SVG paths is actually a series of small commands. In this guide, we'll take a look at each path command and how we can use them to draw icons.",
-    editedAt: "2023-07-04",
-    image: svgPaths,
-  },
-  {
-    slug: `keys-in-framer-motion`,
-    title: "The Power of Keys in Framer Motion",
-    description:
-      "The React key prop is often only used to suppress React warnings, but it's actually a super powerful tool when used together with Framer Motion. In this post, we'll explore how to use it to make some pretty cool animations.",
-    editedAt: "2023-02-22",
-    image: framerMotionKeys,
-  },
-  {
-    slug: `magic-motion`,
-    title: "Inside Framer's Magic Motion",
-    description:
-      "How does Framer Motion make layout changes look seamless? In this post, we're taking a deep dive into FLIP, the technique used by Framer Motion to animate changes in layout without sacrificing performance.",
-    editedAt: "2022-11-15",
-    image: framerMagicMotion,
-  },
-  {
-    slug: `tokenizer`,
-    title: "Rebuilding Babel: The Tokenizer",
-    description:
-      "How do you build a modern JavaScript compiler from scratch? In this post, we'll rebuild the first piece of a compiler: the tokenizer.",
-    editedAt: "2022-02-20",
-    image: tokenizer,
-  },
-  {
-    slug: "https://nan-archive.vercel.app/how-arrays-work",
-    title: "How do Arrays Work?",
-    description:
-      "What goes on under the hood of the most popular data structure? In this post, we'll uncover the secrets of the array by reinventing one ourselves.",
-    editedAt: "2021-11-13",
-    image: array,
-  },
-  {
-    slug: "https://nan-archive.vercel.app/debugger",
-    title: "Building a Debugger",
-    description:
-      "If you want to build your own debugger, where would you start? In this post, we'll take a look at the inner workings of Playground — an online JS debugger.",
-    editedAt: "2021-05-15",
-    image: debuggerImg,
-  },
-  {
-    slug: "https://nan-archive.vercel.app/sliding-window",
-    title: "The Sliding Window Pattern",
-    description: "An interactive look at a classic array algorithm pattern.",
-    editedAt: "2021-03-21",
-    image: slidingWindow,
-  },
-];
+import { posts, XIcon } from "./posts";
 
 function Post({
   post,
@@ -281,22 +207,5 @@ export default function HomePage() {
         </ul>
       </div>
     </div>
-  );
-}
-
-function XIcon() {
-  return (
-    <svg
-      width="16"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="3"
-    >
-      <path d="M17.25 6.75L6.75 17.25"></path>
-      <path d="M6.75 6.75L17.25 17.25"></path>
-    </svg>
   );
 }
