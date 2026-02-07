48b33f28b3456d6a7192c32ad87c9c03d744ba50
Nanda Syahrasyad <nanda.s@hey.com>
2026-02-07 13:34:17 -0800

    small cleanup

--- app/notes/(content)/layout.tsx
+++ app/notes/(content)/layout.tsx
@@ -30,15 +30,6 @@ export default function NotesLayout({
           <Subscribe />
         </div>
       </header>
-      {/* <aside className="py-8 h-screen sticky top-0 grid gap-6 auto-rows-min">
-        <nav className="text-sm">
-          <ul>
-            <li className="pl-8 relative flex items-center">
-              <Link href="/notes/diagram">Diagramming Workflows</Link>
-            </li>
-          </ul>
-        </nav>
-      </aside> */}
       <main className="bg-gray2 ring-1 ring-black/15 w-full max-w-[1500px] mx-auto flex shadow-sm">
         <div className="px-2 border-r border-borderSoft flex flex-col justify-around text-gray4">
           {Array.from({ length: 8 }).map((_, index) => (
