f9a82b0d6d2daef2f9ba4fd8c6d4a2411ff90973
Nanda Syahrasyad <nanda.s@hey.com>
2026-02-07 16:30:51 -0800

    updates

--- app/notes/(content)/diagram/layout.tsx
+++ app/notes/(content)/diagram/layout.tsx
@@ -7,27 +7,32 @@ export default function DiagramLayout({
   children: React.ReactNode;
 }) {
   return (
-    <article className="grid grid-cols-2 [&>*]:col-start-1 auto-rows-min gap-y-4 pl-12 pb-20 relative gap-x-12 leading-relaxed">
+    <article className="grid grid-cols-2 [&>*]:col-start-1 auto-rows-min gap-y-4 pl-12 pb-20 relative gap-x-12 leading-relaxed isolate">
       {/* <div className="absolute top-[72px] right-0 text-right text-sm font-mono text-gray9">
         <p>Nanda_Syahrasyad</p>
         <p>4/Feb/2026</p>
       </div> */}
       <header className="pt-20 pb-16 col-span-2">
         <h1 className="font-serif text-7xl text-balance leading-[1.1]">
-          Diagramming Workflows
+          DOM-Based Diagrams
         </h1>
         <p className="text-gray11 mt-2">
           An approach to visualizing control flow graphs with HTML and CSS
         </p>
       </header>
-      <p className="mb-16">
-        Over the last few weeks, I've been toying with visualizing Cloudflare
-        Workflows, the result of which we launched publicly last week. Here, I
-        want to talk a bit about the process of building the diagram you see on
-        the right; as you might expect, it proved to be quite a bit of a
-        journey.
+      <p>
+        Over the last few weeks, I've been toying with visualizing directed
+        acyclic graphs (DAGs for short)—specifically ones that track the
+        execution of code. After a ton of experimenting, I landed on an approach
+        that primarily uses HTML and CSS for both laying out the nodes and
+        rendering the connectors. The final technique was used in Cloudflare's
+        latest workflows visualization launch.
       </p>
-      <div className="border-l border-borderSoft shrink-0 flex items-center justify-center !col-start-2 row-span-6 -mr-12">
+      <p className="mb-16">
+        In this post, I want to talk more deeply about how it works and why I
+        chose this approach over something like SVGs.
+      </p>
+      <div className="border-l border-borderSoft shrink-0 flex pt-12 justify-center !col-start-2 row-span-6 row-start-2 -mr-12">
         <WorkflowDiagram workflow={workflowManyOdd} />
       </div>
       {children}
